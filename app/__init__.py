71c4b8e8c5c49447ec2ca3c47ac4e25d16265dec
Skikri Hassane <hassaneskikri@gmail.com>
2024-08-14 01:11:21 +0100

    Change to DevelopmentConfig for development environment

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import Config,DevelopmentConfig,ProductionConfig
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(ProductionConfig) # Change to DevelopmentConfig for development environment
 
     from app import routes
     app.register_blueprint(routes.bp)
